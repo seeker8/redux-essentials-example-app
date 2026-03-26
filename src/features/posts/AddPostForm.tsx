a66642151c45ea06c8c8761ff10634b29ce7d4f8
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-03-26 16:15:33 -0400

    add AddPostForm component

---
+++ src/features/posts/AddPostForm.tsx
@@ -0,0 +1,49 @@
+
+interface AddPostFormFields extends HTMLFormControlsCollection {
+  postTitle: HTMLInputElement,
+  postContent: HTMLTextAreaElement
+}
+
+interface AddPostFormElements extends HTMLFormElement {
+  readonly elements: AddPostFormFields
+}
+
+export const AddPostForm = () => {
+
+  const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
+    e.preventDefault();
+
+    const { elements } = e.currentTarget;
+    const title = elements.postTitle.value;
+    const content = elements.postContent.value;
+
+    console.log('Values: ', { title, content });
+
+    e.currentTarget.reset();
+  }
+
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type='text'
+          id='postTitle'
+          name='postTitle'
+          required
+        />
+        <label htmlFor="postContent">Post Content:</label>
+        <textarea
+          id='postContent'
+          name='postContent'
+          defaultValue=''
+          required
+        />
+        <button>Save Post</button>
+      </form>
+    </section>
+  )
+}
+
