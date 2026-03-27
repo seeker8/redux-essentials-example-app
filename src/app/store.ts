4f904faab8fc318773141919b4281fed7c5b31d7
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-03-27 01:13:30 -0400

    add auth state to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import { postsReducer } from "@/features/posts/postsSlice";
 import { userReducer } from "@/features/users/usersSlice";
+import { authReducer } from "@/features/auth/authSlice";
 
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
-    users: userReducer
+    users: userReducer,
+    auth: authReducer
   }
 });
 
